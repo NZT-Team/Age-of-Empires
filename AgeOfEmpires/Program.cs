6e4fa5478445a7b9d09727bb0537cc9aa4ff7963
Nikita <nikita.dmitrovich@gmail.com>
2021-07-07 11:18:05 +0300

    Fin Init Commit

---
+++ AgeOfEmpires/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AgeOfEmpires
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new GameMenu())
+                game.Run();
+        }
+    }
+}
