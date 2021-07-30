a437d95aea2b896fc7a950f04df9f4cdfd764371
bajenob <jonjill1488@gmail.com>
2021-07-30 23:19:43 +0300

    Создал ветвь и добавил в ней пару папок

---
+++ GameGUIComponents/GameGUIComponents/GameMapItem/GameMapItem.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameGUIComponentsLibrary.GameGUIComponents.GameMapItem
+{
+    class GameMapItem
+    {
+    }
+}
--- GameGUIComponents/obj/Debug/netcoreapp3.1/GameGUIComponents.AssemblyInfo.cs
+++ GameGUIComponents/obj/Debug/netcoreapp3.1/GameGUIComponents.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("GameObjectsLibrary")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("GameGUIComponents")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("GameObjectsLibrary")]
-[assembly: System.Reflection.AssemblyTitleAttribute("GameObjectsLibrary")]
+[assembly: System.Reflection.AssemblyProductAttribute("GameGUIComponents")]
+[assembly: System.Reflection.AssemblyTitleAttribute("GameGUIComponents")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
