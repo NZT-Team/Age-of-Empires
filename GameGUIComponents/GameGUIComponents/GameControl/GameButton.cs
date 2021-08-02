1e4933dcd5622c635ff54f44f9240b24a97485bf
bajenob <jonjill1488@gmail.com>
2021-08-03 00:53:25 +0300

    Created Control Classes

---
+++ GameGUIComponents/GameGUIComponents/GameControl/GameButton.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+using Microsoft.
+    
+
+namespace GameGUIComponentsLibrary.GameGUIComponents.GameControl
+{
+    class GameButton : GameButton
+    {
+        Point point;
+        Size size;
+        Color color;
+        
+       
+        
+    }
+}
