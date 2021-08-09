4beb33be99c5e090c48fdf03f62ccd00139c3147
bajenob <jonjill1488@gmail.com>
2021-08-09 21:10:57 +0300

    btnfix

---
+++ GameGUIComponentsLibrary/GameControls/Class1.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+public class Button : Game
+{
+	int buttonX, buttonY;
+    Texture2D texture;
+    MouseState lastMouseState;
+
+    public int ButtonX
+    {
+        get
+        {
+            return buttonX;
+        }
+    }
+
+    public int ButtonY
+    {
+        get
+        {
+            return buttonY;
+        }
+    }
+
+    public Button(string name, Texture2D texture, int buttonX, int buttonY)
+    {
+        this.Name = name;
+        this.Texture = texture;
+        this.buttonX = buttonX;
+        this.buttonY = buttonY;
+    }
+
+    public void Update(GameTime gameTime)
+    {
+        MouseState currentMouseState = Mouse.GetState();
+        if (enterButton() && lastMouseState.LeftButton == ButtonState.Released && currentMouseState.LeftButton == ButtonState.Pressed)
+        {
+            switch (Name)
+            {
+                case "start": 
+                    //startgame
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    public bool enterButton()
+    {
+        if (lastMouseState.X < buttonX + texture.Width &&
+                    lastMouseState.X > buttonX &&
+                    lastMouseState.Y < buttonY + texture.Height &&
+                    lastMouseState.Y > buttonY)
+        {
+            return true;
+        }
+        return false;
+    }
+
+
+
+
+}
