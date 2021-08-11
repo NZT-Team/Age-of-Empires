4cfc82900a26653abe43bdd22050a2fd4db9f9af
bajenob <jonjill1488@gmail.com>
2021-08-11 14:16:20 +0300

    labelfix

--- GameGUIComponentsLibrary/GameControls/LabelControl.cs
+++ GameGUIComponentsLibrary/GameControls/LabelControl.cs
@@ -2,16 +2,24 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 public class Label : Game
 {
 
 	Color color;
-	int labelX, labelY;
+    private int color1Transparent = 150;
+    int labelX, labelY;
 	int Width, Height;
-	string text;
+	string Text;
+    public String DisplayText
+    {
+        get { return Text; }
+        set { Text = value; Invalidate(); }
+    }
 
-	
     public Label(string name, int labelY, int labelX, Color color, int width, int height, string text)
     {
         this.Name = name;
@@ -21,5 +29,13 @@ public class Label : Game
         this.color = color;
         this.Height = height;
         this.Width = width;
+    }
+
+    public void Draw(PaintEventArgs e)
+    {
+        this.Text = text;
+        Color c1 = Color.FromArgb(color1Transparent, color);
+        SolidBrush myBrush = new System.Drawing.SolidBrush(System.Drawing.Color.Red);
+        e.Graphics.DrawString(this.Text, this.Font, myBrush, new Point(0, 0));
     }
 }
